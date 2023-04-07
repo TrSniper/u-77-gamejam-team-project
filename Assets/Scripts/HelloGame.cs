60e7a17537e0a79f1073ba8e54b6bf906ee35e38
Erenalp Teksen <erenalpteksen01@gmail.com>
2023-04-08 01:56:49 +0300

    project created

---
+++ Assets/Scripts/HelloGame.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloGame : MonoBehaviour
+{
+    public void sayHello(){
+        print("Helloooo");
+    }
+}
